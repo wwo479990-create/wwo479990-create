58b294a4969e7052a3532ac4fefa5f3dca275fd4
wwo479990-create <wwo479990@gmail.com>
2026-08-25 09:10:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded',function () {
+  document.getElementById('year').innerHTML = new Date().getFullYear();
     console.log('Portfolio ของ Phumraphi พร้อมแล้ว!');
 });
